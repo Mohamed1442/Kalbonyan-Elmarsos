84b83ba10fe0fb9b1a7af0a358c1c4d1c929746e
Mohamed1442 <mohamedmagdy7428@gmail.com>
2022-09-29 18:28:53 +0200

    linkedin

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing-Game-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing-Game-Challenge.py
@@ -1,1 +1,8 @@
+gussedFood = input("What is your favorite food?")
 
+if gussedFood == "meat":
+  print('Yep! So amazing!')
+else :
+  print('Yuck! That is not it!')
+
+print("Thanks for playing!")
--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Strings-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Strings-challenge.py
@@ -1,1 +1,5 @@
+miles = input('Enter a distance in miles: ')
 
+kilometers_value = float(miles) * 1.609344
+
+print("The value in km is", kilometers_value)
